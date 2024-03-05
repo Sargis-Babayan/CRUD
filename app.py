16b7d66d6d1d5a56bce55d0b46b2124ff99e4f59
neagara <sbabayan133@gmail.com>
2024-03-05 15:20:00 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Task Model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form['title']
+    new_task = Task(title=title)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update_task(id):
+    task = Task.query.get(id)
+    if request.method == 'POST':
+        task.title = request.form['title']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', task=task)
+
+@app.route('/delete/<int:id>')
+def delete_task(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
